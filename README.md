300c4af3f67f5188661334af4603b9bba9974c8c
blob#0005 <88004068+blob0005@users.noreply.github.com>
2022-05-25 16:29:27 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Roblox-Fake-Brute-Forcer
+
+Setup:
+
+1. download python (https://python.org)
+2. open the file that u downloaded
+3. follow the setup
+4. open it agian and click modify
+5. select all and click next
+6. select all and click next
+7. wait until downloaded
+8. click close
+9. run setup.bat
+10. run start.bat
+11. enter amount of robux, boom
+
+#-----------------
+
+Info:
+
+Official Discord: https://discord.gg/Crc92V59ck
+
+developer is blob#0005
+
+if have any problem running program or want me add anything feel free to add my discord blob#0005
---
+++ main.py
@@ -0,0 +1,51 @@
+error = False
+try:
+    import python_percentage
+except:
+    error = True
+if error == True:
+    while True:
+        fix = input("Missing Module, Wanna Try Auto Fix (y/n): ")
+        if fix == "y" or fix == "n":
+            break
+        else:
+            print("Enter A Valid Choice")
+    if fix == "n":
+        exit()
+    if fix == "y":
+        try:
+            import os
+            os.system("pip install python-percentage")
+            print("Problem May Be Fixed Now, Restart Prgoram")
+            input("")
+            exit()
+        except:
+            print("Error Fixing Error")
+            input("")
+            exit()
+def calc():
+    while True:
+        while True:
+            try:
+                e = input("Enter Amount Of Robux: ")
+                e = float(e)
+                break
+            except:
+                print("Enter A Valid Choice")
+        r =  python_percentage.get_percentage(30.0, e)
+        print("Fee: " + str(r))
+        print("")
+        h = float(e) - r
+        print("Owner Will Get: " + str(h))
+        print("")
+        j = float(e) + float(r)
+        print("If Want No Fee Need Pay: " +  str(j))
+        input("")
+        clear()
+def clear():
+    try:
+        import os
+        os.system("cls")
+    except:
+        pass
+calc()
